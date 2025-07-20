a2976a3bf0f4ce6bd6b9b98d5f0ae9dbd962d506
Arnaud <arnaud@icarios.net>
2025-07-20 03:10:44 +0200

    fix(snippets): corrige la non completion des snippets perso

---
+++ lua/plugins/blink.lua
@@ -0,0 +1,8 @@
+return {
+  "saghen/blink.cmp",
+  dependencies = { "L3MON4D3/LuaSnip" },
+  opts = {
+    -- indispensable pour avoir la completion de snippets persos
+    snippets = { preset = "luasnip" },
+  },
+}
