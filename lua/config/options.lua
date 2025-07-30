b98300ff463869adef51b57170444349170cf9b5
Arnaud <arnaud@icarios.net>
2025-07-30 11:40:41 +0200

    feat: essai d'utilisation de formatprg
    Pour l'occasion, je remplace l'antique `par` pour le plus moderne
    `dprint`

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,3 +5,17 @@
 
 vim.g.mapleader = ","
 vim.g.maplocalleader = ";"
+
+-- Programme utilisé pour formater le texte avec `gq`.
+-- Le formatage habituel de vim peut être obtenu avec `gw`.
+--vim.opt.formatprg = "par w55 e1"
+vim.opt.formatprg = "~/.dprint fmt --stdin --config ~/.config/dprint/config.json"
+vim.opt.textwidth = 72
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    vim.opt_local.formatprg = "dprint fmt --stdin --config ~/.config/dprint/config.json"
+    vim.opt_local.textwidth = 72
+  end,
+})
