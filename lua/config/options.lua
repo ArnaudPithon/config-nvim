b8d8dbde588600257e97c425263682dcdb9101bd
icarios <arnaud@icarios.net>
2025-08-07 21:59:58 +0200

    feat(config): retour à plus de simplicité
    - retour à <space> pour mapleader
    - <,> pour maplocalleader
    - suppression de formatprg -> ça ne fonctionne pas comme ça devrait

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,13 +3,8 @@
 -- https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.g.mapleader = ","
-vim.g.maplocalleader = ";"
+vim.g.maplocalleader = ","
 
--- Programme utilisé pour formater le texte avec `gq`.
--- Le formatage habituel de vim peut être obtenu avec `gw`.
---vim.opt.formatprg = "par w55 e1"
-vim.opt.formatprg = "~/.dprint fmt --stdin --config ~/.config/dprint/config.json"
 vim.opt.textwidth = 72
 
 vim.api.nvim_create_autocmd("FileType", {
